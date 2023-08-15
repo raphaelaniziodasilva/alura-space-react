93c13eaaa87447904cf9091288f214d6a0828a4d
Raphael Anizio da Silva <raphaelanizio01@gmail.com>
2023-08-15 16:30:35 -0300

    adicionando banner na pagina principal sem ser pelo componente

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,26 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
 export default function PaginaInicial() {
     return (        
         <>
             <Cabecalho />
-            <Menu />
+
+            {/* Para criar o banner da pagina, eu vou adiciona-lo direto na pagina nao vou criar um componente pois so vai ser uma img */}
+            <main>
+                <section className={styles.principal}>
+
+                <Menu />
+
+                <div className={styles.principal__imagem}>
+                    <h1>A Galeria mais completa do espaço</h1>
+                    <img src={banner} alt="Imagem da terra vista do espaço" />
+                </div>
+
+                </section>
+            </main>
         </>
     )
 }
